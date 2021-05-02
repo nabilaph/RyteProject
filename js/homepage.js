efeed54df189ad9bd7667416988c5dab2d9dbd83
Putri Nabila H <putrinabilahapsari7@gmail.com>
2021-05-03 02:54:34 +0700

    add more ui

--- js/homepage.js
+++ js/homepage.js
@@ -5,15 +5,6 @@ menuToggle.addEventListener('click', function(){
     nav.classList.toggle('slide');
 });
 
-const scrolltop = document.querySelector("#toTop");
-scrolltop.addEventListener("click", function(){
-    window.scrollTo({
-        top: 0,
-        left : 0,
-        behavior: "smooth",
-    });
-});
-
 const scroll = document.querySelector('#navli');
 scroll.addEventListener("click", function(){
     window.scrollTo({
